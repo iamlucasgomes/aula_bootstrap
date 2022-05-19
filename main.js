289e7a9c73420e08cb342fb4bcf88a3cb3ccb41d
Lucas <lucas.devjs@gmail.com>
2022-05-18 22:37:52 -0300

    correção no main.js //linha 1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
